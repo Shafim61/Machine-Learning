3d2bd6649dfafd7b5f263adefb0a112e8c8310a9
Shafim61 <tanvirrashik94@gmail.com>
2021-02-03 18:05:18 +0600

    Iphone price prediction

---
+++ data_visualization.py
@@ -0,0 +1,7 @@
+import pandas
+import matplotlib.pyplot as plt
+
+data = pandas.read_csv("iphone_price.csv")
+plt.scatter(data['version'],data['price'])
+plt.bar(data['version'],data['price'])
+plt.show()
